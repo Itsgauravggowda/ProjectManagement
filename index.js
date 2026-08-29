fa41c8e82a787d836a87c086155fc4df5ffaaaeb
Gaurav <gauravggowda2775@gmail.com>
2026-08-29 15:37:50 +0530

    Create dotenv

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path : "./.env",
+})
+
+
+let database = process.env.database
+let myusername = process.env.userName
+console.log(myusername);
+console.log(database);
+
 console.log("Hello");
