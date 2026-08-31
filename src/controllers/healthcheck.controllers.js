4544b4e37fe2debdc38d4eadef9e93a4fe494e82
Gaurav <gauravggowda2775@gmail.com>
2026-08-31 19:33:35 +0530

    HealthCheck Update

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,13 +1,18 @@
 import {ApiResponse} from "../utils/api-response.js"
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthcheck = (req, res)=>{
+
+/*const healthcheck = (req, res)=>{
     try{
         res
         .status(200)
         .json(new ApiResponse(200, {message: "Server is running"}))
     }
     catch(error){}
-}
+}*/
 
+const healthcheck = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "Server is  running" }));
+});
 
 export {healthcheck};
